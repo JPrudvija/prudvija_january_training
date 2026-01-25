8e35b4e3ac35fd7ec2606abe3e727980a10a0900
JPrudvija <prudvija2006@gmail.com>
2026-01-25 23:53:46 +0530

    Update q3.py

--- assignmet2/q3.py
+++ assignmet2/q3.py
@@ -1,1 +1,22 @@
+Question 2
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, Normalizer
+df = pd.read_csv("encoded_churn_data.csv")
+num_cols = ["Age", "Tenure", "MonthlyCharges", "TotalCharges"]
+# Min-Max Scaling
+minmax = MinMaxScaler()
+df_minmax = minmax.fit_transform(df[num_cols])
 
+# Max Absolute Scaling
+maxabs = MaxAbsScaler()
+df_maxabs = maxabs.fit_transform(df[num_cols])
+
+# Z-Score Standardization
+standard = StandardScaler()
+df_standard = standard.fit_transform(df[num_cols])
+
+# Vector Normalization
+
+normalizer = Normalizer()
+df_normalized = normalizer.fit_transform(df[num_cols])
+print("Feature Scaling Completed")
