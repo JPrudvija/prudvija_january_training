0cf4dac0a6da88c4ad1db6d51f9b1e576990b880
JPrudvija <prudvija2006@gmail.com>
2026-01-25 23:54:47 +0530

    Update q4.py

--- assignmet2/q4.py
+++ assignmet2/q4.py
@@ -1,1 +1,16 @@
+#Question 4
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+df = pd.read_csv("encoded_churn_data.csv")
+# Skewness Transformation
+df["TotalCharges_Log"] = np.log1p(df["TotalCharges"])
 
+# Train-Test Split
+X = df.drop(columns=["Churn", "Churn_LE"])
+y = df["Churn_LE"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+print("Training set:", X_train.shape)
+print("Testing set:", X_test.shape)
